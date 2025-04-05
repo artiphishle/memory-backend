2bc335648a16cc544afd942f4426c15f7f27d4b7
artiphishle <fabio.gartenmann@protonmail.com>
2025-04-05 11:45:53 +0200

    Use custom domain instead of variable generated url per build

--- src/main/java/com/memorygame/config/WebConfig.java
+++ src/main/java/com/memorygame/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://memory-frontend-mjrmkhbnm-artiphishles-projects.vercel.app")
+                .allowedOrigins("https://memory.ankhorage.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
